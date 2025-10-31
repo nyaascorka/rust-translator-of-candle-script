e9ecb92de8f80f31c28201cc388118c510e5ea74
nyaascorka <nyaascorka@gmail.com>
2025-10-31 16:25:37 +0000

    bug

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use iscrolib::candle_script::Token;
+use iscrolib::candle_script::{
+    Token,
+    // Ast,
+};
 
 fn main() {
     // READ FILE AS Vec<char> (UTF_32!)
+    let source_path = std::env::args().nth(1).unwrap_or_else(|| {
+        // eprintln!("Please specify the path. - Dmitriy & Iscra (^_^)");
+        // std::process::exit(-1);
+        "candle-script-samples/index.cdl".to_string()
+    });
     let candle_source = std::fs::read_to_string(
-        "candle-script-samples/index.cdl"
+        source_path
     ).unwrap_or_else(|e| {
-        eprintln!("Comptime error while reading file: {e}.");
+        eprintln!("Comptime error while reading file: {e}. - Dmitriy & Iscra (>_<)");
         std::process::exit(-1);
     })
         .chars()
